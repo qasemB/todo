0e2d2322c75a5542e43d70789aef31a7e343059e
qasemB <mahdicmptr@gmail.com>
2024-10-24 17:38:14 +0330

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Stack />      
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ app/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const home = () => {
+    return (
+        <View>
+            <Text>
+                سلام دنیا
+            </Text>
+        </View>
+    );
+};
+
+export default home;
